226d86fc55df24372725d7b628f0a79953322aac
Max Mukhin <stch.max.muhin@gmail.com>
2018-11-03 10:07:19 +0300

    chore: add user details service for test (user, admin)

--- src/main/java/com/github/stonedch/socialnetwork/config/WebSecurityConfig.java
+++ src/main/java/com/github/stonedch/socialnetwork/config/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.github.stonedch.socialnetwork.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -20,5 +25,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .logout()
                 .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                    .username("user")
+                    .password("user")
+                    .roles("USER")
+                    .build();
+        UserDetails admin =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("admin")
+                        .roles("ADMIN")
+                        .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
