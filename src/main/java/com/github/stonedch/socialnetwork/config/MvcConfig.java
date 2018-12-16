b236789bfa86f9af58bd1ef479ceaa37ad353544
Max Mukhin <stch.max.muhin@gmail.com>
2018-12-16 15:36:09 +0300

    fix: change redirect in mvc config

--- src/main/java/com/github/stonedch/socialnetwork/config/MvcConfig.java
+++ src/main/java/com/github/stonedch/socialnetwork/config/MvcConfig.java
@@ -10,7 +10,7 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
-        registry.addRedirectViewController("/", "/login");
         registry.addViewController("/index").setViewName("index");
+        registry.addRedirectViewController("/", "/index");
     }
 }
