7e86476201ba1b6df40b5c5f6d31ac33145c5358
Max Mukhin <stch.max.muhin@gmail.com>
2019-03-17 12:12:08 +0300

    feat: add account editor

--- src/main/java/com/github/stonedch/socialnetwork/controller/AdminController.java
+++ src/main/java/com/github/stonedch/socialnetwork/controller/AdminController.java
@@ -1,14 +1,22 @@
 package com.github.stonedch.socialnetwork.controller;
 
+import com.github.stonedch.socialnetwork.domain.Account;
+import com.github.stonedch.socialnetwork.domain.Role;
 import com.github.stonedch.socialnetwork.repository.AccountRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/admin")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class AdminController {
     @Autowired
     private AccountRepository accountRepository;
@@ -17,5 +25,36 @@ public class AdminController {
     public String getAdmin(Model model) {
         model.addAttribute("accountList", accountRepository.findAll());
         return "admin";
+    }
+
+    @GetMapping("{account}")
+    public String getAccountEditForm(@PathVariable Account account,
+                                     Model model) {
+        model.addAttribute("account", account);
+        model.addAttribute("roles", Role.values());
+        return "accountEdit";
+    }
+
+    @PostMapping()
+    public String saveAccount(@RequestParam("id") Account account,
+                              @RequestParam("username") String username,
+                              @RequestParam("roles") Set<String> rolesFromForm) {
+        account.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        account.getRoles().clear();
+
+        for (String role : rolesFromForm) {
+            if (roles.contains(role)) {
+                account.getRoles().add(Role.valueOf(role));
+            }
+        }
+
+        accountRepository.save(account);
+
+        return "redirect:/admin";
     }
 }
